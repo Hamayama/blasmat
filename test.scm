fad9e12bfa6c5bd96cf5b0ce7cbb3781af55fac9
Hamayama <hamay1010@gmail.com>
2019-03-17 13:58:52 +0900

    test chg

--- test.scm
+++ test.scm
@@ -23,18 +23,27 @@
 (test* "test-blasmat" "blasmat is working"
        (test-blasmat))
 
-(define A (f64array (shape 0 2 0 3)  1  2  3  4  5  6))
-(define B (f64array (shape 0 2 0 3)  7  8  9 10 11 12))
-
+(define A  (f64array (shape 0 2 0 3)  1  2  3  4  5  6))
+(define B  (f64array (shape 0 2 0 3)  7  8  9 10 11 12))
 (test* "blas-array-daxpy 1" #,(<f64array> (0 2 0 3) 9 12 15 18 21 24)
        (blas-array-daxpy A B 2.0) array-nearly=?)
 
-(define A (f64array (shape 0 2 0 3)  1  2  3  4  5  6))
-(define B (f64array (shape 0 3 0 2)  7  8  9 10 11 12))
-(define C (f64array (shape 0 2 0 2) 10 20 30 40))
+(define G1 (f64array (shape 0 0 0 0)))
+(define G2 (f64array (shape 0 0 0 0)))
+(test* "blas-array-daxpy 2" G1
+       (blas-array-daxpy G1 G2 1.0))
 
+(define A  (f64array (shape 0 2 0 3)  1  2  3  4  5  6))
+(define B  (f64array (shape 0 3 0 2)  7  8  9 10 11 12))
+(define C  (f64array (shape 0 2 0 2) 10 20 30 40))
 (test* "blas-array-dgemm 1" #,(<f64array> (0 2 0 2) 68 84 169 194)
        (blas-array-dgemm A B C 1.0 1.0) array-nearly=?)
+
+(define G1 (f64array (shape 0 0 0 0)))
+(define G2 (f64array (shape 0 0 0 0)))
+(define G3 (f64array (shape 0 0 0 0)))
+(test* "blas-array-dgemm 2" G1
+       (blas-array-dgemm G1 G2 G3 1.0 1.0))
 
 (format (current-error-port) "~%~a" ((with-module gauche.test format-summary)))
 
